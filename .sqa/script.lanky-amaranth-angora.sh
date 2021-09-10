ad89b423653977220e0873fff27385970453af4b
orviz <orviz@ifca.unican.es>
2021-09-10 12:51:17 +0200

    Delete .sqa/script.lanky-amaranth-angora.sh file

--- .sqa/script.lanky-amaranth-angora.sh
+++
@@ -1,3 +0,0 @@
-(
-cd github.com/EOSC-synergy/saps-scheduler && mvn -f /saps-common-repo/pom.xml -DskipTests install&&mvn -f /saps-catalog-repo/pom.xml -DskipTests install&&mvn -f /saps-scheduler-repo/pom.xml spotless:check
-)
